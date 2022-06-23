cb280aef09c7f14f6e0affe79bbbe06a120939af
skhomuti <skhomuti@gmail.com>
2022-06-23 19:06:53 +0500

    Homework updated

--- homework.py
+++ homework.py
@@ -1,15 +1,15 @@
 # Сделайте функцию, которая будет печатать
-# читаемое имя переданной ей функции и ее аргументов.
-# Вызовите ее для трех функций ниже в цикле
+# читаемое имя переданной ей функции и значений аргументов.
+# Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
 
-def open_browser():
+def open_browser(browser_name):
     pass
 
 
-def go_to_companyname_homepage():
+def go_to_companyname_homepage(page_url):
     pass
 
 
-def find_registration_button_on_login_page():
+def find_registration_button_on_login_page(page_url, button_text):
     pass
